77168b2ef4a247748e2314229f3f627c810d2cfc
Malfeasant <mischasumrall@gmail.com>
2014-11-19 15:12:06 -0700

    more band options in comments

--- src/us/malfeasant/spectralyzer/Main.java
+++ src/us/malfeasant/spectralyzer/Main.java
@@ -16,10 +16,13 @@ public class Main {
 	private static final int SAMPLES = 200;
 	
 	private static final float[] FREQ = {
+//		1070, 1270, 2025, 2225, 	// bell 103
 //		55, 110, 220, 440, 880, 1760, 3520	// octaves of A
-		350, 440, 480, 620,	// busy/ring/dial
+//		350, 440, 480, 620,	// busy/ring/dial
 		697, 770, 852, 941, 1209, 1336, 1477, 1633,	// DTMF
-//		261.63f, 277.18f, 293.66f, 311.13f, 329.63f, 349.23f, 369.99f, 392.00f, 415.30f, 440.00f, 466.16f, 493.88f	// Middle C scale
+//		 261.63f,  277.18f,  293.66f,  311.13f,  329.63f,  349.23f,  369.99f,  392.00f,  415.30f,  440.00f,  466.16f,  493.88f,	// Middle C scale
+//		 523.26f,  554.36f,  587.32f,  622.26f,  659.26f,  698.46f,  739.98f,  784.00f,  830.60f,  880.00f,  932.32f,  987.76f,	// C5 scale
+//		1046.52f, 1108.72f, 1174.64f, 1244.52f, 1318.52f, 1396.92f, 1479.96f, 1568.00f, 1661.20f, 1760.00f, 1864.64f, 1975.52f,	// C6 scale
 		};
 	
 	private static final Band[] bands = new Band[FREQ.length];
